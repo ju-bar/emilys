af6cc59074ab7fabe0898d446d6dc4b07ba7b98f
ju-bar <juribarthel@gmail.com>
2019-07-06 01:48:16 +0200

    projection fit with arbitrary order

--- optics/projection.py
+++ optics/projection.py
@@ -14,6 +14,7 @@ published under the GNU General Publishing License, version 3
 from numba import jit # include compilation support
 import numpy as np
 from scipy.special import comb
+from scipy.optimize import minimize
 
 class projection_func_2d:
     '''
@@ -52,6 +53,8 @@ class projection_func_2d:
         self.__num_terms = int((self.max_order + 1) * (self.max_order + 2) / 2 )
         # internal coefficient list
         self.__lcoeff = np.zeros((self.num_terms, 2), dtype=float) # preset to 0
+        # coefficient / term use flags (functions will return zeros for not flagged terms)
+        self.__luse_term = np.full(self.num_terms, 1, dtype=int) # preset to 1
         # coefficient to index list (l,k) -> idx, -1 = no coefficient (not all are used), READ BUT DO NOT WRITE!
         self.__lcoeff_to_idx = np.full((self.max_order + 1,self.max_order + 1), -1, dtype=int)
         # index to coefficient list idx -> (l,k), READ BUT DO NOT WRITE!
@@ -92,6 +95,14 @@ class projection_func_2d:
         '''
         return self.__num_terms
 
+    def get_lk(self, idx):
+        '''
+        Returns the polynomial powers (l,k) for the term of given index.
+        '''
+        if idx >=0 and idx < self.num_terms:
+            return self.__lidx_to_coeff[idx]
+        return np.array([])
+
     @property
     def lcoeff(self):
         '''
@@ -105,11 +116,9 @@ class projection_func_2d:
         Set internal coefficient list.
 
         Parameters:
-            lcoeff : array_like, complex, shape = (self.num_terms,2) or shorter
+            lcoeff : array_like, float, shape = (self.num_terms,2) or shorter
                 polynomial coefficients
 
-        Return:
-            integer, number of coefficients set
         '''
         lc1 = np.atleast_1d(lc)
         assert lc1.dtype in [float,int], 'lcoeff needs numbers!'
@@ -131,10 +140,10 @@ class projection_func_2d:
                 polynomial term coefficients
         '''
         if idx >= 0 and idx < self.num_terms:
-            if alk0 != None and alk0 in [float, int]:
+            if alk0 != None:
                 self.__lcoeff[idx,0] = alk0
-            if alk1 != None and alk1 in [float, int]:
-                self.__lcoeff[idx,0] = alk0
+            if alk1 != None:
+                self.__lcoeff[idx,1] = alk1
 
     def set_coeff_lk(self, l, k, alk0 = None, alk1 = None):
         '''
@@ -150,7 +159,58 @@ class projection_func_2d:
             idx = self.__lcoeff_to_idx[k,l]
             self.set_coeff_idx(idx, alk0, alk1)
 
-    def project(self, x, lcoeff = None):
+    @property
+    def luse(self):
+        '''
+        Returns a copy of the internal usage flag list.
+        '''
+        return self.__luse_term.copy()
+
+    @luse.setter
+    def luse(self, lu):
+        '''
+        Set internal usage flag list.
+
+        Parameters:
+            lu : array_like, int, shape = (self.num_terms,) or shorter
+                polynomial term flags
+
+        '''
+        lu1 = np.atleast_1d(lu)
+        assert lu1.dtype in [float,int], 'luse needs numbers!'
+        nu = np.size(lu1)
+        if nu > 0:
+            lu2 = lu1.flatten().astype(int)
+            self.__luse_term[0:nu] = lu2[0:nu]
+
+    def set_use_idx(self, idx, use=1):
+        '''
+        Sets useage flag for index idx of the current coefficient list (1 by default)
+
+        Parameters:
+            idx : int
+                list index
+            use : int, default: 1
+                polynomial term flag
+        '''
+        if idx >= 0 and idx < self.num_terms:
+            self.__luse_term[idx] = int(use)
+
+    def set_use_lk(self, l, k, use=1):
+        '''
+        Sets useage flag for the term x0**l x1**k (1 by default)
+
+        Parameters:
+            l, k : int
+                polynomial term exponents
+            use : int
+                polynomial term flag
+        '''
+        if l >= 0 and k >= 0 and l+k <= self.max_order:
+            idx = self.__lcoeff_to_idx[k,l]
+            self.set_use_idx(idx, use)
+
+    def project(self, x, lcoeff = np.array([]), luse=np.array([])):
         '''
         Projects x with a 2d polynomial.
 
@@ -159,6 +219,8 @@ class projection_func_2d:
                 position in object plane [...,[xi0,xi1],...]
             lcoeff : array_like, float, optional
                 sequence of projection coefficient interpreted as tuples {[alk0,alk1]}
+            luse : array_like, int, optional
+                polynomial term flags
 
         Return:
             numpy.ndarray
@@ -177,7 +239,8 @@ class projection_func_2d:
             M = projection_func_2d.num_terms
 
         '''
-        if lcoeff != None: self.lcoeff = lcoeff # set new coefficients if present
+        if np.size(lcoeff) > 0: self.lcoeff = lcoeff # set new coefficients if present
+        if np.size(luse) > 0: self.luse = luse # set flags
         nx1 = np.size(x)
         assert 0 == nx1%2 and nx1 > 0, 'project expects an even number of coordinates'
         nx = int(nx1 / 2)
@@ -186,19 +249,21 @@ class projection_func_2d:
         n = self.max_order
         m = self.num_terms
         fpx = np.full((n+1, 2), 1., dtype=float)
-        fpt = np.zeros(m, dtype=float)
+        fpt0 = np.zeros(m, dtype=float)
         for i in range(0, nx): # for all positions
             pwx = lx[i]
             for l in range(1, n+1): # calculate x0**l and x1**l for l = 1 .. n
                 fpx[l] = pwx
                 pwx = pwx * lx[i]
+            fpt = fpt0 # reset terms
             for j in range(0, m): # calculate binomial(l+k,l) x0**l * x1**k for all terms
+                if 0 == self.__luse_term[j]: continue # skip term j
                 l, k = self.__lidx_to_coeff[j]
                 fpt[j] = fpx[l,0] * fpx[k,1] * self.__lbinom[j]
             ly[i] = np.dot(fpt, self.__lcoeff) # project coefficient lists on binomial term list
         return ly
 
-    def project_deriv_lcoeff(self, x):
+    def project_deriv_lcoeff(self, x, luse=np.array([])):
         '''
         2d projection function derivatives with respect to all projection
         parameters (lcoeff). Since the projection is a linear function of
@@ -207,6 +272,8 @@ class projection_func_2d:
         Parameters:
             x : array_like, float, tuple, shape = (N,2)
                 position in object plane [...,[xi0,xi1],...]
+            luse : array_like, int, optional
+                polynomial term flags
 
         Return:
             numpy.ndarray
@@ -225,6 +292,7 @@ class projection_func_2d:
             (M,2) of the parameter list lcoeff. It consumes 4 times more memory than really needed,
             but this should be acceptable since M is usually not very large (<100).
         '''
+        if np.size(luse) > 0: self.luse = luse # set flags
         nx1 = np.size(x)
         assert 0 == nx1%2 and nx1 > 0, 'project expects an even number of coordinates'
         nx = int(nx1 / 2)
@@ -232,21 +300,23 @@ class projection_func_2d:
         n = self.max_order
         m = self.num_terms
         ldy = np.zeros((nx,m,2,2), dtype=float) # prepare result arrays
+        fpt0 = np.zeros(m, dtype=float)
         fpx = np.full((n+1, 2), 1., dtype=float)
-        fpt = np.zeros(m, dtype=float)
         mid = np.array([[[1.],[0.]],[[0.],[1.]]])
         for i in range(0, nx): # for all positions
             pwx = lx[i]
             for l in range(1, n+1): # calculate x0**l and x1**l for l = 1 .. n
                 fpx[l] = pwx
                 pwx = pwx * lx[i]
+            fpt = fpt0 # reset terms
             for j in range(0, m): # calculate tlk(x) = binomial(l+k,l) x0**l * x1**k for all terms
+                if 0 == self.__luse_term[j]: continue # skip term j
                 l, k = self.__lidx_to_coeff[j]
                 fpt[j] = fpx[l,0] * fpx[k,1] * self.__lbinom[j]
             ldy[i] = np.dot(mid,[fpt]).T # writes [...,[[tlk(x),0],[0,tlk(x)]],...]
         return ldy
 
-    def project_deriv_x(self, x, lcoeff=None):
+    def project_deriv_x(self, x, lcoeff=np.array([]), luse=np.array([])):
         '''
         2d projection function derivatives with respect to positions x,
         evaluated for all x.
@@ -256,6 +326,8 @@ class projection_func_2d:
                 position in object plane [...,[xi0,xi1],...]
             lcoeff : array_like, float, optional
                 sequence of projection coefficient interpreted as tuples {[alk0,alk1]}
+            luse : array_like, int, optional
+                polynomial term flags
 
         Return:
             numpy.ndarray
@@ -270,7 +342,8 @@ class projection_func_2d:
             The output of this method may be used as Jacobian for least-squares methods.
         
         '''
-        if lcoeff != None: self.lcoeff = lcoeff # set new coefficients if present
+        if np.size(lcoeff) > 0: self.lcoeff = lcoeff # set new coefficients if present
+        if np.size(luse) > 0: self.luse = luse
         nx1 = np.size(x)
         assert 0 == nx1%2 and nx1 > 0, 'project expects an even number of coordinates'
         nx = int(nx1 / 2)
@@ -279,13 +352,15 @@ class projection_func_2d:
         m = self.num_terms
         ldy = np.zeros((nx,2,2), dtype=float) # prepare result arrays
         fpx = np.full((n+1, 2), 1., dtype=float)
-        fpt = np.zeros((m,2), dtype=float) # prepare derivative terms
+        fptz = np.zeros((m,2), dtype=float) # prepare derivative terms
         for i in range(0, nx): # for all positions
             pwx = lx[i]
             for l in range(1, n+1): # calculate x0**l and x1**l for l = 1 .. n
                 fpx[l] = pwx
                 pwx = pwx * lx[i]
+            fpt = fptz # reset terms
             for j in range(0, m): # calculate binomial(l+k,l) [l * x0**(l-1) * x1**k, k * x0**l * x1**(k-1)] for all terms
+                if 0 == self.__luse_term[j]: continue # skip term j
                 l, k = self.__lidx_to_coeff[j]
                 fpt0 = 0.
                 if l>0: # catch derivative of constant (no element of fpx[l-1] for l-1 = -1)
@@ -297,3 +372,100 @@ class projection_func_2d:
             # project coefficient lists on binomial term derivative list
             ldy[i] = np.dot(fpt.T, self.__lcoeff) # (2,m).(m,2) -> (2,2): [[dyi0/dx0,dyi1/dx0],[dyi0/dx1,dyi1/dx1]]
         return ldy
+
+    # define a square deviation function to minimize deviations between measured and
+    # modelled peak positions using the 2d projection function
+    # prm = [k0, dfobj.lcoeff]
+    def __sdevproj_x0(self,x): # assumes shape(x) = (n,)
+        lc = np.zeros(self.num_terms*2)
+        lc[2:] = x[2:] # copy input
+        self.lcoeff = lc # set input
+        lx = self.__xdata_fitx0 + x[0:2] # (m,2)
+        lwf = self.__wdata_fitx0.flatten() # (2*m)
+        dpmf = (self.project(lx) - self.__ydata_fitx0).flatten() # (2*m,) [dy10,dy11,dy20,dy21,...,dyn0,dyn1]
+        return np.dot(dpmf, dpmf * lwf) / np.sum(lwf) # sum_i=1,m[ sum_a=0,1[ dyia**2 * wi ] ] / sum(wi)
+    # ... and define a Jacobian
+    def __dsdevproj_x0(self,x): # assumes shape(x) = (n,)
+        lc = np.zeros(self.num_terms*2)
+        lc[2:] = x[2:] # copy input
+        self.lcoeff = lc # set input
+        lx = self.__xdata_fitx0 + x[0:2] # (m,2)
+        lwf = self.__wdata_fitx0.flatten() # (2*m)
+        dpm = (self.project(lx) - self.__ydata_fitx0) # (m,2) : [[dy10,dy11],[dy20,dy21],...,[dym0,dym1]
+        dx0 = self.project_deriv_x(lx) # (m,2,2) : [...,[[ddyi0/dk00, ddyi1/dk00], [ddyi0/dk01 + ddyi1/dk01],...]
+        sdx0 = dx0.shape
+        dx0s = np.reshape(dx0,(sdx0[0],1,sdx0[1],sdx0[2])) # (m,1,2,2)
+        dcoeff = self.project_deriv_lcoeff(lx) # (m,n/2,2,2) : [...,[[ddyi0/daj0,ddyi1/daj0],[ddyi0/daj1,ddyi1/daj1]],...]
+        dp = dcoeff # (m,n/2,2,2)
+        sdp = dp.shape
+        dp[:,0,:,:] = dx0s[:,0,:,:] # (m,n/2,2,2) (replace y shift derivatives by x shift dervatives)
+        # ... combine dp and dpm to # (n/2,2) : [...,sum_i[dyi0*ddyi0/daj0 + dyi1*ddyi1/daj0, dyi0*ddyi0/daj1 + dyi1*ddyi1/daj1],...]
+        # The common dimensions are m = number of samples and 2, number space dimensions.
+        dps = np.transpose(dp,(1,3,0,2)).reshape(sdp[1],sdp[3],sdp[0]*sdp[2]) # (n/2,2,2*m)
+        ds2 = 2. * np.dot(dps, dpm.flatten() * lwf).flatten() / np.sum(lwf) # (n,) 
+        return ds2
+
+    def fit_x0_lcoeff(self, xdata, ydata, ysigm=np.array([]), x0=np.array([]), lcoeff0=np.array([]), luse=np.array([])):
+        '''
+        Fits the projection to a set of data including an x0 shift
+        instead of the shift coefficients (order 0) using a minimum
+        least-squares approach.
+
+        Parameters:
+            xdata : array_like, shape=(m,2)
+                object plane position data
+            ydata : array_like, shape=(m,2)
+                image plane position data
+            ysigm : array_like, shape=(m,2), optional
+                image plane position error
+            x0 : tuple, optional
+                x data shift, initializes to [0,0] of not given
+            lcoeff : array_like, float, optional
+                sequence of projection coefficient interpreted as tuples {[alk0,alk1]}
+                initializes to the internal coefficient list of the object if not given
+            luse : array_like, int, optional
+                polynomial term flags 
+
+        '''
+        #
+        lcbk = self.lcoeff #  backup coefficients
+        lubk = self.luse # backup use flags
+        if np.size(lcoeff0) > 0: self.lcoeff = lcoeff0
+        if np.size(luse) > 0: self.luse = luse
+        self.set_coeff_idx(0, 0., 0.) # no shift
+        self.set_use_idx(0, 0) # no shift
+        n = self.num_terms
+        prm0 = np.zeros(2*n, dtype=float)
+        if np.size(x0)>=2: prm0[0:2] = x0[0:2]
+        prm0[2:n] = self.lcoeff.flatten()[2:n] # copy initial parameters (except shift)
+        # data
+        m2 = min(np.size(xdata),np.size(ydata))
+        m = int((m2 - m2%2) / 2) # number of data tuples
+        # store data lists as class member
+        self.__xdata_fitx0 = np.array(xdata).flatten()[0:2*m].reshape(m,2)
+        self.__ydata_fitx0 = np.array(ydata).flatten()[0:2*m].reshape(m,2)
+        self.__wdata_fitx0 = np.full((m,2), 1.)
+        # setup weights from y error estimates
+        if np.size(ysigm) >= m2:
+            ys = np.array(np.abs(ysigm)).flatten()[0:2*m].reshape(m,2)
+            for i in range(0, m):
+                if ys[i,0] > 0.:
+                    self.__wdata_fitx0[i,0] = 1./ys[i,0]**2
+                if ys[i,1] > 0.:
+                    self.__wdata_fitx0[i,1] = 1./ys[i,1]**2
+                wmax = np.max(self.__wdata_fitx0)
+                if wmax > 0:
+                    if ys[i,0] == 0.:
+                        self.__wdata_fitx0[i,0] = wmax
+                    if ys[i,1] == 0.:
+                        self.__wdata_fitx0[i,1] = wmax
+        # minimize square deviations
+        nmsol = minimize(self.__sdevproj_x0, prm0, method='BFGS', jac=self.__dsdevproj_x0)
+        prmf = nmsol.x
+        prmcov = nmsol.hess_inv
+        sqrdev = self.__sdevproj_x0(prmf)
+        # restore backup
+        self.lcoeff = lcbk # coefficients
+        self.luse = lubk # use flags
+        # return fit result
+        return [prmf, prmcov, sqrdev]
