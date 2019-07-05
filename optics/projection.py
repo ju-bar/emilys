952720449772047923a684d1033163411bfd310e
ju-bar <juribarthel@gmail.com>
2019-07-05 17:51:18 +0200

    projection derivatives to least-squares

--- optics/projection.py
+++ optics/projection.py
@@ -38,6 +38,11 @@ class projection_func_2d:
         + ...
         = sum_l sum_k binomial(l+k,l) * x0**l * x1**k * [alk0 , alk1]
 
+    The linear coefficients are listed in lcoeff in the sequence
+    [...,[a100,a101],[a010,a011],...]. A linear distortion matrix
+    corresponding to [[a100,a010],[a101,a011]] must therefore be
+    entered in transposed form.
+
     '''
     #
     def __init__(self, max_order):
@@ -150,14 +155,29 @@ class projection_func_2d:
         Projects x with a 2d polynomial.
 
         Parameters:
-            x : array_like, float, tuple
-                position in object plane
-            lcoeff : array_like, float
+            x : array_like, float, tuple, shape = (N,2)
+                position in object plane [...,[xi0,xi1],...]
+            lcoeff : array_like, float, optional
                 sequence of projection coefficient interpreted as tuples {[alk0,alk1]}
 
-        You may set the coefficients before calling the project method
-        using projection_func_2d.lcoeff = lcoeff.
+        Return:
+            numpy.ndarray
+                [...,[yi0,yi1],...]
+                list of projected positions as array of shape(N, 2) with
+                N = number of tuples in parameter x = numpy.size(x)/2
+
+        Remarks:
+            You may set the coefficients before calling the project method
+            using projection_func_2d.lcoeff = lcoeff.
+
+            The function calculates for each tuple [xi0,xi1]:
+            [yi0,yi1] = sum_j=1,M [alk0, alk1] * tlk(xi0,xi1),
+            with tlk(xi0,xi1) = binomial(l+k,l) * x0**l * x1**k
+            and an internal listing j :-> l,k for each j in range(0, M).
+            M = projection_func_2d.num_terms
+
         '''
+        if lcoeff != None: self.lcoeff = lcoeff # set new coefficients if present
         nx1 = np.size(x)
         assert 0 == nx1%2 and nx1 > 0, 'project expects an even number of coordinates'
         nx = int(nx1 / 2)
@@ -172,8 +192,108 @@ class projection_func_2d:
             for l in range(1, n+1): # calculate x0**l and x1**l for l = 1 .. n
                 fpx[l] = pwx
                 pwx = pwx * lx[i]
-            for j in range(0, m): # calculate binomial(l+k,l) x0**l * x1**k for all orders
+            for j in range(0, m): # calculate binomial(l+k,l) x0**l * x1**k for all terms
                 l, k = self.__lidx_to_coeff[j]
                 fpt[j] = fpx[l,0] * fpx[k,1] * self.__lbinom[j]
             ly[i] = np.dot(fpt, self.__lcoeff) # project coefficient lists on binomial term list
         return ly
+
+    def project_deriv_lcoeff(self, x):
+        '''
+        2d projection function derivatives with respect to all projection
+        parameters (lcoeff). Since the projection is a linear function of
+        the parameters, the result depends on x only.
+
+        Parameters:
+            x : array_like, float, tuple, shape = (N,2)
+                position in object plane [...,[xi0,xi1],...]
+
+        Return:
+            numpy.ndarray
+                [...,[...,[[dyi0/dalk0, dyi1/dalk0],[dyi0/dalk1, dyi1/dalk1]],...],...]
+                list of derivatives as numpy.ndarray of shape(N, M, 2, 2) with
+                N = number of tuples in parameter x
+                M = number of projection terms tlk(xi0,xi1) = binomial(l+k,l) * x0**l * x1**k
+
+        Remarks:
+            The output of this method may be used as Jacobian for least-squares methods.
+            Since
+                dyi0/dalk0 = dyi1/dalk1 = tlk(xi0,xi1), and
+                dyi1/dalk0 = dyi0/dalk1 = 0,
+            the 2 x 2 items of the output are [[1,0],[0,1]] * tlk(xi0,xi1).
+            This redundancy produces the correct shape of the derivative array for the shape
+            (M,2) of the parameter list lcoeff. It consumes 4 times more memory than really needed,
+            but this should be acceptable since M is usually not very large (<100).
+        '''
+        nx1 = np.size(x)
+        assert 0 == nx1%2 and nx1 > 0, 'project expects an even number of coordinates'
+        nx = int(nx1 / 2)
+        lx = np.reshape(x,(nx,2))
+        n = self.max_order
+        m = self.num_terms
+        ldy = np.zeros((nx,m,2,2), dtype=float) # prepare result arrays
+        fpx = np.full((n+1, 2), 1., dtype=float)
+        fpt = np.zeros(m, dtype=float)
+        mid = np.array([[[1.],[0.]],[[0.],[1.]]])
+        for i in range(0, nx): # for all positions
+            pwx = lx[i]
+            for l in range(1, n+1): # calculate x0**l and x1**l for l = 1 .. n
+                fpx[l] = pwx
+                pwx = pwx * lx[i]
+            for j in range(0, m): # calculate tlk(x) = binomial(l+k,l) x0**l * x1**k for all terms
+                l, k = self.__lidx_to_coeff[j]
+                fpt[j] = fpx[l,0] * fpx[k,1] * self.__lbinom[j]
+            ldy[i] = np.dot(mid,[fpt]).T # writes [...,[[tlk(x),0],[0,tlk(x)]],...]
+        return ldy
+
+    def project_deriv_x(self, x, lcoeff=None):
+        '''
+        2d projection function derivatives with respect to positions x,
+        evaluated for all x.
+
+        Parameters:
+            x : array_like, float, tuple, shape = (N,2)
+                position in object plane [...,[xi0,xi1],...]
+            lcoeff : array_like, float, optional
+                sequence of projection coefficient interpreted as tuples {[alk0,alk1]}
+
+        Return:
+            numpy.ndarray
+                [...,[[dyi0/dx0,dyi1/dx0],[dyi0/dx1,dyi1/dx1]],...]
+                list of derivatives as array of shape(N, 2, 2) with
+                N = number of tuples in parameter x
+        
+        Remarks:
+            You may set the coefficients before calling the project_deriv_x method
+            using projection_func_2d.lcoeff = lcoeff.
+
+            The output of this method may be used as Jacobian for least-squares methods.
+        
+        '''
+        if lcoeff != None: self.lcoeff = lcoeff # set new coefficients if present
+        nx1 = np.size(x)
+        assert 0 == nx1%2 and nx1 > 0, 'project expects an even number of coordinates'
+        nx = int(nx1 / 2)
+        lx = np.reshape(x,(nx,2))
+        n = self.max_order
+        m = self.num_terms
+        ldy = np.zeros((nx,2,2), dtype=float) # prepare result arrays
+        fpx = np.full((n+1, 2), 1., dtype=float)
+        fpt = np.zeros((m,2), dtype=float) # prepare derivative terms
+        for i in range(0, nx): # for all positions
+            pwx = lx[i]
+            for l in range(1, n+1): # calculate x0**l and x1**l for l = 1 .. n
+                fpx[l] = pwx
+                pwx = pwx * lx[i]
+            for j in range(0, m): # calculate binomial(l+k,l) [l * x0**(l-1) * x1**k, k * x0**l * x1**(k-1)] for all terms
+                l, k = self.__lidx_to_coeff[j]
+                fpt0 = 0.
+                if l>0: # catch derivative of constant (no element of fpx[l-1] for l-1 = -1)
+                    fpt0 = l * fpx[l-1,0] * fpx[k,1] * self.__lbinom[j] # (dtj/dx0)
+                fpt1 = 0.
+                if k>0: # catch derivative of constant (no element of fpx[k-1] for k-1 = -1)
+                    fpt1 = k * fpx[l,0] * fpx[k-1,1] * self.__lbinom[j] # (dtj/dx1)
+                fpt[j] = np.array([fpt0,fpt1]) # set derivative tuple of term j
+            # project coefficient lists on binomial term derivative list
+            ldy[i] = np.dot(fpt.T, self.__lcoeff) # (2,m).(m,2) -> (2,2): [[dyi0/dx0,dyi1/dx0],[dyi0/dx1,dyi1/dx1]]
+        return ldy
