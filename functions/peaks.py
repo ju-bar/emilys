ff9c9272d19bdcc4667d919ea79e95f7745bc27b
ju-bar <juribarthel@gmail.com>
2019-07-13 13:38:18 +0200

    bug fixes in local area grabber

--- functions/peaks.py
+++ functions/peaks.py
@@ -3,7 +3,7 @@
 Created on Fri Jul 12 15:57:00 2019
 @author: ju-bar
 
-Peak functions
+Peak functions and functions calculating their jacobians
 
 This code is part of the 'emilys' repository
 https://github.com/ju-bar/emilys
@@ -26,6 +26,37 @@ def gauss_2d(x_tuple, x0, y0, a, bxx, bxy, byy, c):
     fyy = (byy * byy + bxy * bxy)
     varg = fxx * dx**2 + 2 * fxy * dx * dy + fyy * dy**2
     return a * np.exp(-varg) + c
+def gauss_2d_jac(x_tuple, x0, y0, a, bxx, bxy, byy, c):
+    '''
+    Derivatives of
+        a * Exp[ - (bxx*bxx + bxy*bxy) * (x-x0)**2 
+             - 2 * bxy * (bxx + byy) * (x-x0) * (y-y0)
+             - (byy*byy + bxy*bxy) * (y-y0)**2 ] + c
+    with respect to parameters x0, y0, a, bxx, bxy, byy, c
+    Returns
+        numpy.array([dx0, dy0, da, dbxx, dbxy, dbyy, dc]).T
+    computed for all M input x_tuple sets, where x0 etc. can
+    be arrays of length M
+    '''
+    (x, y) = x_tuple
+    dx = x - x0
+    dy = y - y0
+    fxx = (bxx * bxx + bxy * bxy)
+    fxy = 2 * bxy * (bxx + byy)
+    fyy = (byy * byy + bxy * bxy)
+    dx2 = dx**2
+    dy2 = dy**2
+    dxy = dx * dy
+    varg = fxx * dx2 + 2 * fxy * dxy + fyy * dy2
+    vexp = np.exp(-varg)
+    dx0 = a * vexp * 2 * (fxx * dx + fxy * dy)
+    dy0 = a * vexp * 2 * (fxy * dx + fyy * dy)
+    da = vexp
+    dbxx = -2 * a * vexp * (bxx * dx2 + bxy * dxy)
+    dbxy = -2 * a * vexp * (bxy * (dx2 + dy2) + (bxx + byy) * dxy)
+    dbyy = -2 * a * vexp * (bxy * dxy + byy * dy2)
+    dc = np.full(np.size(x), 1., dtype=float)
+    return np.array([dx0, dy0, da, dbxx, dbxy, dbyy, dc]).T
 # %%
 def gauss_2d_round(x_tuple, x0, y0, a, b, c):
     '''
@@ -36,6 +67,28 @@ def gauss_2d_round(x_tuple, x0, y0, a, b, c):
     dy = y - y0
     varg = b**2 * (dx**2 + dy**2)
     return a * np.exp(-varg) + c
+def gauss_2d_round_jac(x_tuple, x0, y0, a, b, c):
+    '''
+    Derivatives of 
+        a * exp[ -b**2 * ((x-x0)**2 + (y-y0)**2) ] + c
+    with respect to parameters x0, y0, a, b, c
+
+    Returns
+        numpy.array([dx0, dy0, da, db, dc]).T
+    computed for all M input x_tuple sets, where x0 etc. can
+    be arrays of length M
+    '''
+    (x, y) = x_tuple
+    dx = x - x0
+    dy = y - y0
+    varg = b**2 * (dx**2 + dy**2)
+    vexp = np.exp(-varg)
+    dx0 = a * vexp * 2 * b**2 * dx
+    dy0 = a * vexp * 2 * b**2 * dy
+    da = vexp
+    db = -2 * vexp * b * (dx**2 + dy**2)
+    dc = np.full(np.size(x), 1., dtype=float)
+    return np.array([dx0, dy0, da, db, dc]).T
 # %%
 def gauss_2d_round_slope(x_tuple, x0, y0, a, b, c, dx, dy):
     '''
@@ -45,4 +98,29 @@ def gauss_2d_round_slope(x_tuple, x0, y0, a, b, c, dx, dy):
     (x, y) = x_tuple
     gr = gauss_2d_round(x_tuple, x0, y0, a, b, c)
     return gr + dx*(x-x0) + dy*(y-y0)
+def gauss_2d_round_slope_jac(x_tuple, x0, y0, a, b, c, dx, dy):
+    '''
+    Derivatives of 
+        a * exp[-b**2 * ((x-x0)**2 + (y-y0)**2)] + c
+        + dx*(x-x0) + dy*(y-y0)
+    with respect to parameters x0, y0, a, b, c, dx, dy
+
+    Returns
+        numpy.array([dx0, dy0, da, db, dc, ddx, ddy]).T
+    computed for all M input x_tuple sets, where x0 etc. can
+    be arrays of length M
+    '''
+    (x, y) = x_tuple
+    mx = x - x0
+    my = y - y0
+    varg = b**2 * (mx**2 + my**2)
+    vexp = np.exp(-varg)
+    dx0 = a * vexp * 2 * b**2 * mx
+    dy0 = a * vexp * 2 * b**2 * my
+    da = vexp
+    db = -2 * vexp * b * (mx**2 + my**2)
+    dc = np.full(np.size(x), 1., dtype=float)
+    ddx = mx
+    ddy = my
+    return np.array([dx0, dy0, da, db, dc, ddx, ddy]).T
 # %%
--- image/peakfit.py
+++ image/peakfit.py
@@ -12,7 +12,7 @@ published under the GNU General Publishing License, version 3
 """
 import numpy as np
 from scipy.optimize import curve_fit
-from emilys.functions.peaks import gauss_2d
+import emilys.functions.peaks as pks
 # %%
 def get_data_in_circroi(image, pos, rad, wrap=True):
     '''
@@ -42,7 +42,7 @@ def get_data_in_circroi(image, pos, rad, wrap=True):
     irad = np.ceil(np.abs(rad)) + 1
     ir0 = icent - np.array([irad,irad], dtype=int)
     ir1 = icent + np.array([irad,irad], dtype=int)
-    ndat = (ir1[0] - ir1[0] + 1) * (ir1[1] - ir1[1] + 1)
+    ndat = (ir1[0] - ir0[0] + 1) * (ir1[1] - ir0[1] + 1)
     datx = np.zeros((ndat,2), dtype=float)
     daty = np.zeros(ndat, dtype=float)
     nfit = 0
@@ -55,7 +55,7 @@ def get_data_in_circroi(image, pos, rad, wrap=True):
                 continue # skip this row
         dy = k - pos[1]
         dy2 = dy**2
-        for h in range(ir0[0], ir1[1]+1):
+        for h in range(ir0[0], ir1[0]+1):
             ih = h
             if wrap:
                 ih = int( h%ndim[1] )
@@ -67,12 +67,68 @@ def get_data_in_circroi(image, pos, rad, wrap=True):
             if (d <= np.abs(rad) and nfit < ndat):
                 datx[nfit] = np.array([h,k])
                 daty[nfit] = image[ik,ih]
-                nfit += 1
+                nfit = nfit + 1
     if (nfit > 0):
         return [nfit, datx[0:nfit], daty[0:nfit]]
     return [0, [], []]
 # %%
-def fit_local_gauss_2d(image, pos, rad, wrap=True):
+def get_values_in_circroi(image, pos, rad, wrap=True):
+    '''
+    Extracts values in a circular region of interest from an image.
+
+    Parameters:
+        image : numpy.ndarray
+            image data
+        pos : array (x,y)
+            center position for the fit region of interest
+        rad : float
+            radius of the fit region of interest
+            must be larger than 2
+        wrap : bool
+            flag for using periodic wrap around when crossing
+            input image bounds
+
+    Return:
+        [number of image points n, image values]
+        [int, numpy.ndarray (n)]
+    '''
+    ndim = np.array(image.shape)
+    assert ndim.size == 2, 'expecting a 2d array as parameter 1'
+    assert np.size(pos) == 2, 'expecting a tuple as parameter 2'
+    assert np.abs(rad) > 2, 'expecting rad > 2 (parameter 3)'
+    icent = np.array(pos).astype(int)
+    irad = np.ceil(np.abs(rad)) + 1
+    ir0 = icent - np.array([irad,irad], dtype=int)
+    ir1 = icent + np.array([irad,irad], dtype=int)
+    ndat = (ir1[0] - ir0[0] + 1) * (ir1[1] - ir0[1] + 1)
+    daty = np.zeros(ndat, dtype=float)
+    nfit = 0
+    for k in range(ir0[1], ir1[1]+1):
+        ik = k
+        if wrap:
+            ik = int( k%ndim[0] )
+        else:
+            if (k < 0 or k >= ndim[0]):
+                continue # skip this row
+        dy = k - pos[1]
+        dy2 = dy**2
+        for h in range(ir0[0], ir1[0]+1):
+            ih = h
+            if wrap:
+                ih = int( h%ndim[1] )
+            else:
+                if (h < 0 or h >= ndim[1]):
+                    continue # skip this columns
+            dx = h - pos[0]
+            d = np.sqrt(dy2 + dx**2)
+            if (d <= np.abs(rad) and nfit < ndat):
+                daty[nfit] = image[ik,ih]
+                nfit = nfit + 1
+    if (nfit > 0):
+        return [nfit, daty[0:nfit]]
+    return [0, []]
+# %%
+def fit_local_gauss_2d(image, pos, rad, wrap=True, imagesigma=None):
     '''
     Fits a gaussian peak to a circular local image area.
 
@@ -99,9 +155,15 @@ def fit_local_gauss_2d(image, pos, rad, wrap=True):
     assert np.size(pos) == 2, 'expecting a tuple as parameter 2'
     assert np.abs(rad) > 2, 'expecting rad > 2 (parameter 3)'
     (nfit, xfit, yfit) = get_data_in_circroi(image, pos, rad, wrap)
-    assert nfit > 2 * nprm, 'insufficient number of image points in region of interest'
+    assert nfit > 2 * nprm, 'insufficient number of image points '\
+        '({:d} of {:d}) in region of interest'.format(nfit, 2*nprm)
+    ysig = None
+    if (imagesigma != None): # also extract image sigma values
+        (nfit2, ysig) = get_values_in_circroi(imagesigma, pos, rad, wrap)
+        assert nfit == nfit2, 'internal data size conflict with sigma data'
     prm0 = [ pos[0], pos[1], np.max(yfit) - np.min(yfit), 
              1./np.abs(rad), 0., 1./np.abs(rad), np.min(yfit)] # initial parameter set
-    solprm, solcov = curve_fit(gauss_2d, xfit.T, yfit, prm0) # call scipy.optimize.curve_fit
+    solprm, solcov = curve_fit( pks.gauss_2d, xfit.T, yfit, prm0,
+                                jac=pks.gauss_2d_jac, sigma=ysig) # call scipy.optimize.curve_fit
     solerr = np.sqrt(np.diag(solcov))
     return [solprm, solerr]
