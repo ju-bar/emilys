fb6e6a74586ec3f3a675774d50d9b7e372e195de
ju-bar <juribarthel@gmail.com>
2022-11-08 15:49:30 +0100

    commenting in dpc.py

--- image/dpc.py
+++ image/dpc.py
@@ -14,6 +14,27 @@ published under the GNU General Publishing License, version 3
 import numpy as np
 
 def dpc_image(a, b, c, d, alpha = 0.0):
+    """
+
+    Calculates DPC images from for segmented STEM images a, b, c, d
+    under rotation alpha in the diffraction plane.
+
+    Parameters
+    ----------
+
+        a, b, c, d : numpy.ndarray, (dimension = 2)
+            segmented STEM images
+        alpha : float
+            rotation angle [radians]
+
+    Returns
+    -------
+
+        numpy.ndarray (complex)
+            real part is the x DPC component
+            imaginary part is the y DPC component
+
+    """
     dx = (a - c).astype(np.float64)
     dy = (b - d).astype(np.float64)
     z = np.empty(dx.shape, dtype=np.complex128)
@@ -24,6 +45,33 @@ def dpc_image(a, b, c, d, alpha = 0.0):
     return z
 
 def idpc_image(a, b, c, d, alpha=0., rk = [0., 1.], fp = 2.):
+    """
+
+    Calculates an iDPC image from for segmented STEM images a, b, c, d
+    under rotation alpha in the diffraction plane and band-pass filter.
+
+    Parameters
+    ----------
+
+        a, b, c, d : numpy.ndarray, (dimension = 2)
+            segmented STEM images
+        alpha : float, default 0.
+            rotation angle [radians]
+        rk : list (len=2), default [0., 1.]
+            band pass filter threshold in fractions of the
+            image Nyquist frequency
+        fp : float, default 2.
+            frequency filter exponential power
+            formula: exp(-1.0 * (k2 / thr)**fp)
+
+    Returns
+    -------
+
+        numpy.ndarray (complex)
+            real part is the x DPC component
+            imaginary part is the y DPC component
+
+    """
     nd = a.shape
     pf = -1.J / (2. * np.pi)
     kx = np.tile([np.fft.fftfreq(nd[1])],(nd[0],1))
